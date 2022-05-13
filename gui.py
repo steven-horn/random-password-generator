6034154b5c5d33f8a350ff7c8fe041957e93a35a
steven-horn <shorn.5294@gmail.com>
2022-05-13 14:11:40 -0500

    Adjusted size of window. Added entry box for characters that the user may want/need to omit from the password. Adjusted the password generator to iterate over the length allowing for possible duplicates and eliminating the characters that were chosen to be omitted.

--- gui.py
+++ gui.py
@@ -4,11 +4,11 @@ from functions import *
 
 class Gui:
     def __init__(self, window):
-        """Initial setup for the GUI. The screen will consist of a welcome label, a password length entry, a generate
-        button, and an error message for inputting an incorrect password length."""
+        """Initial setup for the GUI. The screen will consist of a welcome label, a password length entry, a character
+        omission entry, a generate button, and an error message for inputting an incorrect password length."""
 
         self.window = window
-        self.window.geometry('300x300')
+        self.window.geometry('350x300')
         self.window.resizable(False, False)
         self.window.title('Random Password Generator')
 
@@ -21,6 +21,16 @@ class Gui:
         self.length_label.pack(side='left')
         self.length_entry.pack(side='right')
         self.frame_length.pack()
+
+        self.frame_omit = Frame(self.window, highlightbackground='black', highlightthickness=1)
+        self.omit_label = Label(self.frame_omit, text='Characters to omit:')
+        self.omit_entry = Entry(self.frame_omit)
+        self.omit_label.pack(side='left')
+        self.omit_entry.pack(side='right')
+        self.frame_omit.pack()
+
+        self.instruction_label = Label(text='Please separate characters you wish to omit with a comma.')
+        self.instruction_label.pack()
 
         self.generate_button = Button(text='Generate', command=self.generate)
         self.generate_button.pack()
@@ -35,12 +45,16 @@ class Gui:
         self.error_frame.pack_forget()
 
     def generate(self):
+        """Pulls data from the entry boxes in the GUI and puts those values through length (x) and password generator
+        (x,y). Will then delete entries into length and omit."""
         x = length(self.length_entry.get())
+        y = self.omit_entry.get().split(',')
         if x is False:
             self.error_frame.pack()
             self.length_entry.delete(0, END)
         else:
-            self.password_label.config(text=password_generator(x))
+            self.password_label.config(text=password_generator(x, y))
             self.password_label.pack()
             self.length_entry.delete(0, END)
+            self.omit_entry.delete(0, END)
             self.error_frame.pack_forget()
